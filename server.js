3f4f73772421a7ced4816ba3672cbdbec8d9b46c
cosuoji <qelvinz@gmail.com>
2025-07-27 17:56:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use(express.urlencoded({ extended: true })); // For form data
 
   const allowedOrigins = [
 	'http://localhost:5173',
+	'http://localhost:5174',
 	'https://thecompanytesting.netlify.app',
   ];
   
