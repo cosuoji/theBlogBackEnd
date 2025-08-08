8118cdcc7f61ecf014382beaec2b3952a5e268af
cosuoji <qelvinz@gmail.com>
2025-08-08 14:20:08 +0100

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -177,7 +177,7 @@ const generateTokens = (userId) => {
   };
 
 const storeRefreshToken = async (userId, refreshToken) => {
-	await redis.set(`refresh_token:${userId}`, refreshToken, "EX", 7 * 24 * 60 * 60); // 7days
+	await redis.set(`refresh_token:${userId}`, refreshToken, "EX", 30 * 24 * 60 * 60); // 7days
 };
 
 // Update cookie settings
